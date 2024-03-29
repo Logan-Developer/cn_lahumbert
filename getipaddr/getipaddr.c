e2a5ffbc872e07e485c7169202363bad7a028fcf
Logan-Developer <logan2.humbert@gmail.com>
2024-03-29 09:56:07 -0600

    getipaddr: Add a bunch of options

--- getipaddr/getipaddr.c
+++ getipaddr/getipaddr.c
@@ -4,13 +4,81 @@
 #include <string.h>
 #include <stdlib.h>
 
+int showUDP = 0;
+int showIPv4 = 1;
+int showIPv6 = 1;
+
+void printHelp()
+{
+    fprintf(stderr, "Usage: getipaddr <hostname> [-u]\n");
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  -u: UDP instead of TCP\n");
+    fprintf(stderr, "  -v [4|6]: IPV4 or IPV6\n");
+    fprintf(stderr, "  -h: Show this help message\n");
+}
+
 int checkArgs(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+        printHelp();
         return 1;
     }
+    // check that first argument is a hostname
+    if (argv[1][0] == '-')
+    {
+        fprintf(stderr, "Invalid hostname: %s\n", argv[1]);
+        printHelp();
+        return 1;
+    }
+
+    for (int i = 2; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-u") == 0)
+        {
+            showUDP = 1;
+        }
+        else if (strcmp(argv[i], "-v") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "Missing argument for -v\n");
+                printHelp();
+                return 1;
+            }
+
+            if (strcmp(argv[i + 1], "4") == 0)
+            {
+                // IPv4
+                showIPv6 = 0;
+            }
+            else if (strcmp(argv[i + 1], "6") == 0)
+            {
+                // IPv6
+                showIPv4 = 0;
+            }
+            else
+            {
+                fprintf(stderr, "Invalid argument for -v: %s\n", argv[i + 1]);
+                printHelp();
+                return 1;
+            }
+
+            i++;
+        }
+        else if (strcmp(argv[i], "-h") == 0)
+        {
+            printHelp();
+            return 1;
+        }
+        else
+        {
+            fprintf(stderr, "Unknown option: %s\n", argv[i]);
+            printHelp();
+            return 1;
+        }
+    }
+
     return 0;
 }
 
@@ -25,8 +93,20 @@ int main(int argc, char *argv[])
 
     // set hints
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC; // Both IPv4 and IPv6 are allowed
-    hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+    hints.ai_socktype = showUDP ? SOCK_DGRAM : SOCK_STREAM; // SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+
+    if (showIPv4 && showIPv6)
+    {
+        hints.ai_family = AF_UNSPEC; // AF_UNSPEC for both IPv4 and IPv6
+    }
+    else if (showIPv4)
+    {
+        hints.ai_family = AF_INET; // AF_INET for IPv4
+    }
+    else
+    {
+        hints.ai_family = AF_INET6; // AF_INET6 for IPv6
+    }
 
     // get address info
     int status = getaddrinfo(argv[1], NULL, &hints, &addressInfo);
